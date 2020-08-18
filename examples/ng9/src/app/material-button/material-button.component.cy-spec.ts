ea4c59b6a623800a06983608b10bae242b08893c
LeJeanbono <jean.michel.lec@gmail.com>
2020-08-18 19:17:27 +0200

    Fix material test

--- examples/ng9/src/app/material-button/material-button.component.cy-spec.ts
+++ examples/ng9/src/app/material-button/material-button.component.cy-spec.ts
@@ -1,16 +1,18 @@
 import { initEnv, mount, setConfig } from 'cypress-angular-unit-test';
 import { MaterialButtonComponent } from './material-button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MaterialButtonComponent', () => {
 
   beforeEach(() => {
     setConfig({ cssFile: 'node_modules/@angular/material/prebuilt-themes/indigo-pink.css' });
-    initEnv(MaterialButtonComponent, { imports: [MatButtonModule] });
+    initEnv(MaterialButtonComponent, { imports: [MatButtonModule, BrowserAnimationsModule] });
   });
 
   it.skip('should create', () => {
-    mount(MaterialButtonComponent);
+    const fixture = mount(MaterialButtonComponent);
+    fixture.detectChanges();
   });
 
 });
