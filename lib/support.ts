0bb2449f40cca1cd9df31ca6557a78a8297bc4bc
LeJeanbono <jean.michel.lec@gmail.com>
2020-11-08 22:57:40 +0100

    support file

--- lib/support.ts
+++ lib/support.ts
@@ -1,22 +1,20 @@
 /// <reference types="cypress" />
-require('zone.js/dist/zone');
+import 'zone.js/dist/zone';
 
 // @ts-ignore
-const isComponentSpec = () => Cypress.spec.specType === 'component'
+const isComponentSpec = () => Cypress.spec.specType === 'component';
 
 // When running component specs, we cannot allow "cy.visit"
 // because it will wipe out our preparation work, and does not make much sense
 // thus we overwrite "cy.visit" to throw an error
 Cypress.Commands.overwrite('visit', (visit, ...args) => {
   if (isComponentSpec()) {
-    throw new Error(
-      'cy.visit from a component spec is not allowed',
-    )
+    throw new Error('cy.visit from a component spec is not allowed');
   } else {
     // allow regular visit to proceed
-    return visit(...args)
+    return visit(...args);
   }
-})
+});
 
 beforeEach(() => {
   const html = `
@@ -28,7 +26,8 @@ beforeEach(() => {
       <root0 id="root"></root0>
     </body>
   `;
+  // @ts-ignore
   const document = cy.state('document');
   document.write(html);
   document.close();
-})
+});
