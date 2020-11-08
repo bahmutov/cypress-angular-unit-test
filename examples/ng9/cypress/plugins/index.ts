499f55d28797474468f1bfeb0c5e274f58ff2f34
LeJeanbono <jean.michel.lec@gmail.com>
2020-11-08 20:55:41 +0100

    ng9 plugins

--- examples/ng9/cypress/plugins/index.ts
+++ examples/ng9/cypress/plugins/index.ts
@@ -1,8 +1,8 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+import * as cypressTypeScriptPreprocessor from './cy-ts-preprocessor';
+import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/plugin';
 
 module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config);
   on('file:preprocessor', cypressTypeScriptPreprocessor);
   return config;
-}
+};
