5f6dbebdce561e77a8e51ebe429b33ef22cf2f63
LeJeanbono <jean.michel.lec@gmail.com>
2020-11-09 01:01:33 +0100

    Revert "support file"
    This reverts commit 0bb2449f40cca1cd9df31ca6557a78a8297bc4bc.

--- support.js
+++ support.js
@@ -1,20 +1,22 @@
 /// <reference types="cypress" />
-import 'zone.js/dist/zone';
+require('zone.js/dist/zone');
 
 // @ts-ignore
-const isComponentSpec = () => Cypress.spec.specType === 'component';
+const isComponentSpec = () => Cypress.spec.specType === 'component'
 
 // When running component specs, we cannot allow "cy.visit"
 // because it will wipe out our preparation work, and does not make much sense
 // thus we overwrite "cy.visit" to throw an error
 Cypress.Commands.overwrite('visit', (visit, ...args) => {
   if (isComponentSpec()) {
-    throw new Error('cy.visit from a component spec is not allowed');
+    throw new Error(
+      'cy.visit from a component spec is not allowed',
+    )
   } else {
     // allow regular visit to proceed
-    return visit(...args);
+    return visit(...args)
   }
-});
+})
 
 beforeEach(() => {
   const html = `
@@ -26,8 +28,7 @@ beforeEach(() => {
       <root0 id="root"></root0>
     </body>
   `;
-  // @ts-ignore
   const document = cy.state('document');
   document.write(html);
   document.close();
-});
+})
