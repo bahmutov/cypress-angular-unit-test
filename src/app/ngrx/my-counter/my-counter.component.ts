6a367760175925b365534d99d47d0e3c1ecc4589
Jean-Michel Leclercq <jean.michel.lec@gmail.com>
2021-07-07 11:04:58 +0200

    NGRX example (#516)

---
+++ src/app/ngrx/my-counter/my-counter.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { increment, decrement, reset } from '../counter.actions';
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+})
+export class MyCounterComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+}
